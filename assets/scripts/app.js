58b0ea1bbb6c5751c150624fbd265a490fafdfe2
Andrii Kapran (Work Acc) <a.kapran@do-it.co>
2023-01-15 16:41:07 +0100

    code cleanup

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,6 +2,7 @@ class DOMHelper {
   static clearEventListener(elem) {
     const clone = elem.cloneNode(true);
     elem.replaceWith(clone);
+
     return clone;
   }
 
@@ -14,11 +15,12 @@ class DOMHelper {
 }
 
 class Component {
-  host;
+  #hostSelector;
+  #insertBefore = false;
 
   constructor(hostId, insertBefore = false) {
-    this.host = hostId ? document.getElementById(hostId) : document.body;
-    this.insertBefore = insertBefore;
+    this.#hostSelector = hostId ? document.getElementById(hostId) : document.body;
+    this.#insertBefore = insertBefore;
   }
 
   destroy() {
@@ -28,8 +30,8 @@ class Component {
   }
 
   init() {
-    this.host.insertAdjacentElement(
-      this.insertBefore ? "afterbegin" : "beforeend",
+    this.#hostSelector.insertAdjacentElement(
+      this.#insertBefore ? "afterbegin" : "beforeend",
       this.element
     );
   }
@@ -44,60 +46,47 @@ class Tooltip extends Component {
     super(id);
     this.#text = textContent;
     this.closePrevTooltip = closeTooltipFunc;
-    this.create();
+    this.#create();
   }
 
-  closeTooltip = () => {
+  #closeTooltip = () => {
     this.destroy();
     this.closePrevTooltip();
   };
 
-  create() {
+  #create() {
     const elem = document.createElement("div");
     elem.className = "card";
 
-    const hostPosLeft = this.host.offsetLeft;
-    const hostPosTop = this.host.offsetTop;
-    const hostPosHeight = this.host.clientHeight;
-    const scrollHeight = this.host.parentElement.scrollTop;
-
-    const x = hostPosLeft + 20;
-    const y = hostPosTop + hostPosHeight - 10 - scrollHeight;
-
-    elem.style.position = "absolute";
-    elem.style.left = `${x}px`;
-    elem.style.top = `${y}px`;
-
     const templateEl = document.querySelector("#tooltip");
     const templateContent = document.importNode(templateEl.content, true);
-    templateContent.querySelector('p').textContent = this.#text;
-    elem.append(templateContent)
+    templateContent.querySelector("p").textContent = this.#text;
+    elem.append(templateContent);
 
-    // console.log(this.host.getBoundingClientRect());
-    // elem.textContent = this.#text;
-    elem.addEventListener("click", this.closeTooltip);
+    elem.addEventListener("click", this.#closeTooltip);
     this.element = elem;
   }
 }
 
 class ProjectItem {
   id = null;
-  type = "";
-  updateProjListFuncHandler;
+  #type = "";
+  #updateProjListFuncHandler;
   hasTooltip = false;
 
   constructor(id, updateProjListFunc, type) {
     this.id = id;
-    this.type = type;
-    this.updateProjListFuncHandler = updateProjListFunc;
-    this.getMoreInfo();
-    this.switchProjectHandler(this.type);
+    this.#type = type;
+    this.#updateProjListFuncHandler = updateProjListFunc;
+    this.#getMoreInfo();
+    this.#switchProjectHandler(this.#type);
   }
 
   #showModeInfoHandler() {
     if (this.hasTooltip) {
       return;
     }
+
     const el = document.querySelector(`#${this.id}`);
     const text = el.dataset && el.dataset.extraInfo ? el.dataset.extraInfo : "";
 
@@ -106,57 +95,59 @@ class ProjectItem {
     this.hasTooltip = true;
   }
 
-  getMoreInfo() {
+  #getMoreInfo() {
     const elem = document.querySelector(`#${this.id}`);
     let moreInfoBtn = elem.querySelector("button:first-of-type");
     moreInfoBtn = DOMHelper.clearEventListener(moreInfoBtn);
     moreInfoBtn.addEventListener("click", this.#showModeInfoHandler.bind(this));
   }
 
-  switchProjectHandler(type) {
+  #switchProjectHandler(type) {
     const elem = document.querySelector(`#${this.id}`);
     let switchBtn = elem.querySelector("button:last-of-type");
+
     switchBtn = DOMHelper.clearEventListener(switchBtn);
     switchBtn.textContent = type === "active" ? "Finished" : "Activate";
+
     switchBtn.addEventListener(
       "click",
-      this.updateProjListFuncHandler.bind(null, this.id)
+      this.#updateProjListFuncHandler.bind(null, this.id)
     );
   }
 
   update(updateProjListFn, type) {
-    this.updateProjListFuncHandler = updateProjListFn;
-    this.switchProjectHandler(type);
+    this.#updateProjListFuncHandler = updateProjListFn;
+    this.#switchProjectHandler(type);
   }
 }
 
 class ProjectsList {
-  projects = [];
-  switchHandler;
+  #projects = [];
+  #switchHandler;
 
   constructor(type) {
     this.type = type;
     const items = document.querySelectorAll(`#${type}-projects li`);
     for (const item of items) {
-      this.projects.push(
+      this.#projects.push(
         new ProjectItem(item.id, this.switchProject.bind(this), this.type)
       );
     }
   }
 
   setSwitchHandlerFunc(switchHandlerFunc) {
-    this.switchHandler = switchHandlerFunc;
+    this.#switchHandler = switchHandlerFunc;
   }
 
   addProject(project) {
-    this.projects.push(project);
+    this.#projects.push(project);
     DOMHelper.moveElement(project.id, `#${this.type}-projects ul`);
     project.update(this.switchProject.bind(this), this.type);
   }
 
   switchProject(projectId) {
-    this.switchHandler(this.projects.find((p) => p.id === projectId));
-    this.projects = this.projects.filter((p) => p.id !== projectId);
+    this.#switchHandler(this.#projects.find((p) => p.id === projectId));
+    this.#projects = this.#projects.filter((p) => p.id !== projectId);
   }
 }
 
