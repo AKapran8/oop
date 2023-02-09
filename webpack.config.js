a35b836ff675c1390e268f451d8038be8b0e47af
Andrii Kapran (Work Acc) <a.kapran@do-it.co>
2023-02-09 20:04:35 +0100

    changed file names

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	mode: 'development',
 	entry: './src/app.js',
 	output: {
-		filename: 'app.js',
+		filename: '[contenthash].js',
 		path: path.resolve(__dirname, 'assets', 'scripts'),
 		publicPath: 'assets/scripts/'
 	},
